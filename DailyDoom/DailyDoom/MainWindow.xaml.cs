90523e744c70072b337916b2ea89fbc2522e9b08
Abigail Herron <abigailherron@hotmail.com>
2021-09-16 09:45:40 +0100

    Initial Project Commit
    Made sure the window starts in center screen (XAML) and added basic comment outlines to the XAML and CS files

---
+++ DailyDoom/DailyDoom/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿/*##########################################################################################################################################################################
+ * Name: Abigail Herron
+ * ID: S00200536
+ * Year: 3
+ * Date: 19/09/21
+ * 
+ * Description: Contains the MainWindow logic for the DailyDoom App
+ * Properties:
+ * Event-Based Methods:
+ * Logic-Based Methods:
+##########################################################################################################################################################################*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DailyDoom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
